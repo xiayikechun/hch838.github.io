621276b600b7d1887d1eafb86b574645e6b2d915
hch838 <hch838@163.com>
2023-02-10 01:02:31 +0800

    1

---
+++ pwa/sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = `temperature-converter-v1`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+  })());
+});
